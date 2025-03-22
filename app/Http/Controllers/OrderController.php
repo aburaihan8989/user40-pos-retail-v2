d43743d8349ee6300964be7aa8f355cb33cd3cbc
aburaihan8989 <yusup.smartphone@gmail.com>
2025-03-22 13:03:24 +0700

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -30,10 +30,47 @@ class OrderController extends Controller
     public function show($id)
     {
         $order = \App\Models\Order::with('kasir')->findOrFail($id);
+        $kasir = \App\Models\User::where('id', $order->kasir_id)->first();
 
         //get order items by order id
         $orderItems = \App\Models\OrderItem::with('product')->where('order_id', $id)->get();
+        $orderSum = \App\Models\OrderItem::where('order_id', $id)->select(DB::raw('SUM(quantity * total_price) as total'))->value('total');
 
-        return view('pages.orders.view', compact('order', 'orderItems'));
+        return view('pages.orders.view', compact('order', 'kasir', 'orderItems', 'orderSum'));
     }
+
+    public function view(Request $request)
+    {
+        // $orders = \App\Models\Order::with('kasir')->orderBy('transaction_time', 'desc')->paginate(10);
+        // ->whereDate('order_items.created_at',date('Y-m-d',strtotime("today")))
+
+        //get data orders
+        $orders = DB::table('order_items')
+        ->when($request->input('created_at'), function ($query, $created_at) {
+            return $query->where('order_items.created_at', 'like', '%' . $created_at . '%');
+            // return $query->whereDate('order_items.created_at',date('Y-m-d',strtotime("today")));
+        })
+        ->leftJoin('products','products.id','=','order_items.product_id')
+        ->select('products.name',
+             DB::raw('SUM(order_items.quantity) as count'),
+             DB::raw('SUM(order_items.quantity * products.price) as total'))
+        ->groupBy('products.name')
+        ->orderBy('total','desc')
+        ->paginate(10);
+        //sort by transaction_time desc
+
+        // $top_sales = DB::table('order_items')
+        // ->leftJoin('products','products.id','=','order_items.product_id')
+        // ->select('products.name',
+        //      DB::raw('SUM(order_items.quantity) as count'),
+        //      DB::raw('SUM(order_items.quantity * products.price) as total'))
+        // ->groupBy('products.name')
+        // ->orderBy('count','desc')
+        // ->limit(5)
+        // ->get();
+
+
+        return view('pages.orders.view2', compact('orders'));
+    }
+
 }
--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -135,7 +135,7 @@
                                                     <div class="d-flex">
                                                         @if ( auth()->user()->roles == "admin" )
                                                             <a href='{{ route('product.edit', $product->id) }}'
-                                                                class="btn btn-sm btn-info btn-icon">
+                                                                class="btn btn-sm btn-primary btn-icon">
                                                                 <i class="fas fa-edit"></i>
                                                                 Edit
                                                             </a>
@@ -147,16 +147,23 @@
                                                             </a>
                                                         @endif
 
-                                                        <form action="#">
-                                                        {{-- <form action="{{ route('product.destroy', $product->id) }}"
-                                                            method="POST" class="ml-2"> --}}
-                                                            {{-- <input type="hidden" name="_method" value="DELETE" />
-                                                            <input type="hidden" name="_token"
-                                                                value="{{ csrf_token() }}" /> --}}
-                                                            <button class="btn btn-sm btn-secondary btn-icon confirm-delete ml-2">
-                                                                <i class="fas fa-times"></i> Delete
-                                                            </button>
-                                                        </form>
+                                                        @if ( auth()->user()->roles == "admin" )
+                                                            <form action="{{ route('product.destroy', $product->id) }}"
+                                                                method="POST" class="ml-2">
+                                                                <input type="hidden" name="_method" value="DELETE" />
+                                                                <input type="hidden" name="_token"
+                                                                    value="{{ csrf_token() }}" />
+                                                                    <button class="btn btn-sm btn-danger btn-icon confirm-delete ml-2">
+                                                                    <i class="fas fa-times"></i> Delete
+                                                                </button>
+                                                            </form>
+                                                        @else
+                                                            <a href='#'
+                                                                class="btn btn-sm btn-secondary btn-icon ml-2">
+                                                                <i class="fas fa-times"></i>
+                                                                Delete
+                                                            </a>
+                                                        @endif
                                                     </div>
                                                 </td>
                                             </tr>
